827a2f250942bd876dcf5b37ab0df78f29d6bcc5
VicAlar <77180750+VicAlar@users.noreply.github.com>
2022-09-12 18:50:11 -0400

    Actualización en la ruta principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       {
-        path: 'dashboard',
+        path: '',
         loadChildren: () =>
           import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
       },
