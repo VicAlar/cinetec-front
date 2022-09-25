fe08cbdf9340e03f2f710911a7479265036812ac
VicAlar <valfonsob98@gmail.com>
2022-09-25 17:27:10 -0400

    Creación de funciones para las películas

--- src/app/Cinema/detalle-pelicula/detalle-pelicula.module.ts
+++ src/app/Cinema/detalle-pelicula/detalle-pelicula.module.ts
@@ -3,15 +3,34 @@ import {CommonModule} from '@angular/common';
 
 import {DetallePeliculaRoutingModule} from './detalle-pelicula-routing.module';
 import {DetallePeliculaComponent} from './detalle-pelicula.component';
+import {DialogModule} from "primeng/dialog";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {InputNumberModule} from "primeng/inputnumber";
+import {DropdownModule} from "primeng/dropdown";
+import {CalendarModule} from "primeng/calendar";
+import {ToastModule} from "primeng/toast";
 
 
 @NgModule({
   declarations: [
     DetallePeliculaComponent
   ],
-  imports: [
-    CommonModule,
-    DetallePeliculaRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        DetallePeliculaRoutingModule,
+        DialogModule,
+        ButtonModule,
+        RippleModule,
+        InputTextModule,
+        FormsModule,
+        InputNumberModule,
+        DropdownModule,
+        ReactiveFormsModule,
+        CalendarModule,
+        ToastModule
+    ]
 })
 export class DetallePeliculaModule { }
